6e20701f87bf543c222449415958015def7ed0c7
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2023-03-25 12:12:46 -0300

    Revert "Features relacionadas aos posts da minha parte -> MASTER"

--- src/controllers/Posts.js
+++ src/controllers/Posts.js
@@ -1,4 +1,4 @@
-import { deletePostById, editPostById, getAllPosts, getUserPostsById } from "../repositories/Posts.js";
+import { getAllPosts } from "../repositories/Posts.js";
 
 async function getAllUsersPosts(req, res) {
     try {
@@ -10,29 +10,4 @@ async function getAllUsersPosts(req, res) {
     }
 }
 
-async function getUserPosts(req, res) {
-    try {
-        const { id } = req.params;
-        if (!id || Number.isNaN(Number(id))) return res.status(401).send("invalid user id format");
-        const posts = await getUserPostsById(id);
-        return res.status(200).send(posts.rows);
-    } catch(_) {
-        console.log(_);
-        return res.status(500).send("internal server error.");
-    }
-}
-
-async function removePost(req, res) {
-    const { id } = req.params;
-    await deletePostById(id);
-    return res.status(200).send("post deleted successfully.");
-}
-
-async function editPost(req, res) {
-    const { id } = req.params;
-    const { post } = req.body;
-    await editPostById(id, post);
-    return res.status(200).send("post edited successfully.");
-}
-
-export { editPost, getAllUsersPosts, getUserPosts, removePost };
+export { getAllUsersPosts };
--- src/controllers/Users.js
+++ src/controllers/Users.js
@@ -1,19 +1,18 @@
 import { getUserAndUserPostsById } from "../repositories/Posts.js";
-import { getUsersByUsername, getUserInfo, findUserById } from "../repositories/Users.js";
+import { getUsersByUsername, getUserInfo } from "../repositories/Users.js";
 
-async function getUserById(req, res) {
+async function getUserAndUserPosts(req, res) {
     try {
         const { id } = req.params;
         if (!id || Number.isNaN(Number(id))) return res.status(401).send("invalid user id format");
-        const user = await findUserById(id);
-        if (user.rows.length === 0) return res.status(400).send("user not found");
-        else return res.status(200).send(user.rows[0]);
-    } catch (_) {
+        const posts = await getUserAndUserPostsById(id);
+        return res.status(200).send(posts.rows);
+    } catch(_) {
         return res.status(500).send("internal server error.");
     }
 }
 
-async function searchUsersByUsername(req, res) {
+async function searchUsersByUsername() {
     try {
         const { username } = req.body;
         if (!username || typeof username !== "string") return res.status(401).send("invalid username format");
